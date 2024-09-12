0f2133a7ddb084cd57192a3c68a68f6dbd60313a
az0bottle <1553359439@qq.com>
2024-09-12 22:11:37 +0800

    临时commit，希望连到本地服务器

--- src/api/login.js
+++ src/api/login.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(username, password) {
   return request({
-    url: '/admin/login',
+    url: '/auth/login',
     method: 'post',
     data: {
       username,
